dd2521efcba219df2df0d44bd3380af9aaf0a663
Adam Sorrenti <adam.sorrenti@ryerson.ca>
2019-01-07 13:38:13 -0500

    Update RestoreConnection.ps1
    Optimized

--- RestoreConnection.ps1
+++ RestoreConnection.ps1
@@ -1,24 +1,7 @@
 ﻿# Creates a new registry key or updates the existing RestoreConnection key in order to stop mapped network drives from auto connection on start up.
 # This script makes persistent network drives only attempt connection when opened in explorer.
 
-param([switch]$Elevated)
-
-function Test-Admin {
-  $currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
-  $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
-}
-
-if ((Test-Admin) -eq $false)  {
-    if ($elevated) 
-    {
-        # tried to elevate, did not work, aborting
-    } 
-    else {
-        Start-Process powershell.exe -Verb RunAs -ArgumentList ('-windowstyle hidden -noprofile -noexit -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition))
-    }
-
-    exit
-}
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-windowstyle hidden -NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
 
 #Now running with as admin
 
